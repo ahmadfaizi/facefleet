06e2f2595ac193b2b6b5db9bf3f7a28381678898
ahmadfaizi <110796014+ahmadfaizi@users.noreply.github.com>
2024-05-04 10:41:07 +1000

    Update streamlit_app.py
    hello world

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,13 @@
-print('hello')
+import streamlit as st
+from PIL import Image
+
+# Create a file uploader widget
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png"])
+
+# Check if a file has been uploaded
+if uploaded_file is not None:
+    # Open the image file
+    image = Image.open(uploaded_file)
+    
+    # Display the uploaded image
+    st.image(image, caption='Uploaded Image.', use_column_width=True)
